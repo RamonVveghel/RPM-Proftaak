c640ad5f7f87862083082aa9017f9cea82901667
Melanie Hoogenboom <86021@roc-teraa.nl>
2021-06-01 14:11:11 +0200

    Update style/recepten.php

--- RPM-Project/recepten.php
+++ RPM-Project/recepten.php
@@ -5,6 +5,156 @@
   include("inc/navbar.php");
 ?>
 
+<!-- Start main container -->
+<div class="main">
+  <h3 class="text-uppercase">Recepten</h3>
+
+  <div class="custom-container">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm">
+          <div class="recipe-container">
+            Ontbijt
+            </br>
+            Een heerlijk ontbijtje
+          </div>
+        </div>
+        <div class="col-sm">
+        <div class="recipe-container">
+            Ontbijt
+            </br>
+            Een heerlijk ontbijtje
+          </div>
+        </div>
+        <div class="col-sm">
+        <div class="recipe-container">
+            Onbijt
+            </br>
+            Een heerlijk ontbijtje
+          </div>
+        </div>
+      </div>
+      <button type="button" class="btn btn-secondary">Zie meer</button>
+    </div>
+  </div>
+
+  <div class="custom-container">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm">
+          <div class="recipe-container">
+            Lunch
+            </br>
+            Een heerlijke lunch
+          </div>
+        </div>
+        <div class="col-sm">
+        <div class="recipe-container">
+            Lunch
+            </br>
+            Een heerlijke lunch
+          </div>
+        </div>
+        <div class="col-sm">
+        <div class="recipe-container">
+            Lunch
+            </br>
+            Een heerlijke lunch
+          </div>
+        </div>
+      </div>
+      <button type="button" class="btn btn-secondary">Zie meer</button>
+    </div>
+  </div>
+
+  <div class="custom-container">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm">
+          <div class="recipe-container">
+            Diner
+            </br>
+            Een prettige diner
+          </div>
+        </div>
+        <div class="col-sm">
+        <div class="recipe-container">
+            Diner
+            </br>
+            Een prettige diner
+          </div>
+        </div>
+        <div class="col-sm">
+        <div class="recipe-container">
+            Diner
+            </br>
+            Een prettige diner
+          </div>
+        </div>
+      </div>
+      <button type="button" class="btn btn-secondary">Zie meer</button>
+    </div>
+  </div>
+
+  <div class="custom-container">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm">
+          <div class="recipe-container">
+            Dessert
+            </br>
+            Een heerlijke lunch
+          </div>
+        </div>
+        <div class="col-sm">
+        <div class="recipe-container">
+            Dessert
+            </br>
+            Een heerlijke lunch
+          </div>
+        </div>
+        <div class="col-sm">
+        <div class="recipe-container">
+            Dessert
+            </br>
+            Een heerlijke lunch
+          </div>
+        </div>
+      </div>
+      <button type="button" class="btn btn-secondary">Zie meer</button>
+    </div>
+  </div>
+
+  <div class="custom-container">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm">
+          <div class="recipe-container">
+            Lunch
+            </br>
+            Een heerlijke lunch
+          </div>
+        </div>
+        <div class="col-sm">
+        <div class="recipe-container">
+            Lunch
+            </br>
+            Een heerlijke lunch
+          </div>
+        </div>
+        <div class="col-sm">
+        <div class="recipe-container">
+            Lunch
+            </br>
+            Een heerlijke lunch
+          </div>
+        </div>
+      </div>
+    </div>
+    <button type="button" class="btn btn-secondary">Zie meer</button>
+  </div>
+</div>
+
 <?php
   // Get the HTML footer section
   include("inc/footer.php");
