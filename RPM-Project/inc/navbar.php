20a736fea3919876561a160630e5b79764a5688e
Melanie Hoogenboom <86021@roc-teraa.nl>
2021-06-01 12:11:33 +0200

    update navbar

--- RPM-Project/inc/navbar.php
+++ RPM-Project/inc/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-customnavbar">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
     <img src="img/Logo-RPM.png" alt="">
@@ -9,7 +9,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav m-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="#">Home</a>
+          <a class="nav-link" href="index.php">Home</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Recepten</a>
@@ -23,24 +23,32 @@
             <li><a class="dropdown-item" href="#">Dessert</a></li>
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="#">Snacks</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="recepten.php">Alle recepten</a></li>
           </ul>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Build your menu</a>
+          <a class="nav-link" href="buildyourmenu.php">Build your menu</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Blog</a>
+          <a class="nav-link" href="blog.php">Blog</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Over ons</a>
+          <a class="nav-link" href="overons.php">Over ons</a>
         </li>
         <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
         </form>
       </ul>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+        <a class="nav-link" href="#">Login</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#">Register</a>
+        </li>
+      </ul>
     </div>
-    <a href="http://">Inloggen</a>
-    <a href="http://">Registreren</a>
   </div>
 </nav>
