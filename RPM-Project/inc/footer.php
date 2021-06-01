29de40b70d30ff4439c6318e67c743a88d67d928
Melanie Hoogenboom <86021@roc-teraa.nl>
2021-06-01 12:11:28 +0200

    update footer

--- RPM-Project/inc/footer.php
+++ RPM-Project/inc/footer.php
@@ -1,5 +1,67 @@
-<footer>
-        &copy; RPM 2021
+<footer class="bg-footer text-center text-white ">
+<!-- Footer Links -->
+<div class="container-fluid text-center text-md-left">
+  <!-- Grid row -->
+  <div class="row">
+    <!-- Grid column -->
+    <div class="col-md-6 mt-md-0 mt-3">
+      <!-- Content -->
+      <img src="img/Logo--RPM-ZW.png" alt="">
+    </div>
+    <!-- Grid column -->
+    <hr class="clearfix w-100 d-md-none pb-3">
+    <!-- Grid column -->
+    <div class="col-md-3 mb-md-0 mb-3">
+      <!-- Links -->
+      <h4 class="text-uppercase">Recepten</h4>
+      <ul class="list-unstyled">
+        <li>
+          <a>Ontbijt</a>
+        </li>
+        <li>
+          <a>Lunch</a>
+        </li>
+        <li>
+          <a>Diner</a>
+        </li>
+        <li>
+          <a>Dessert</a>
+        </li>
+        <li>
+          <a>Snacks</a>
+        </li>
+      </ul>
+    </div>
+    <!-- Grid column -->
+    <!-- Grid column -->
+    <div class="col-md-3 mb-md-0 mb-3">
+      <!-- Links -->
+      <h4 class="text-uppercase">Contact</h4>
+      <ul class="list-unstyled">
+        <li>
+          <a>Broekwal 30</a>
+        </li>
+        <li>
+          <a href="https://www.google.nl/maps/place/Broekwal+30,+5701+CZ+Helmond/@51.4800646,5.673003,17z/data=!3m1!4b1!4m5!3m4!1s0x47c72394d3be6907:0xe74d8b1cd3a80657!8m2!3d51.4800646!4d5.6751917">5701 CZ Helmond</a>
+        </li>
+        <li>
+          <a>06-54872819</a>
+        </li>
+        <li>
+          <a>RPM-info@gmail.com</a>
+        </li>
+      </ul>
+    </div>
+    <!-- Grid column -->
+  </div>
+  <!-- Grid row -->
+</div>
+<!-- Footer Links -->
+<!-- Copyright -->
+<div class="footer-copyright text-center py-2 ">© 2021 Copyright:
+  <a> RPM</a>
+</div> 
+<!-- Copyright -->
 </footer>
 </body>
 </html>
