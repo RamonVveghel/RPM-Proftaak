2db2ce5caf8417bb0749f2ee82e1c2680985e954
Philip <85866@roc-teraa.nl>
2021-06-01 14:10:57 +0200

    Update Enkel categorie

---
+++ RPM-Project/categorie.php
@@ -0,0 +1,24 @@
+<?php
+  // Get the HTML head section
+  include("inc/header.php");
+
+  include("inc/navbar.php");
+?>
+
+<div class="main">
+    <h3 class="text-uppercase">Ontbijt</h3>
+
+    <div class="custom-container">
+        <div class="custom-row">
+            
+        </div>
+    </div>
+    <div class="custom-container">
+        <div class="custom-row">
+            
+        </div>
+    </div>
+<?php
+  // Get the HTML footer section
+  include("inc/footer.php");
+?>
--- RPM-Project/inc/navbar.php
+++ RPM-Project/inc/navbar.php
@@ -14,7 +14,7 @@
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Recepten</a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Ontbijt</a></li>
+            <li><a class="dropdown-item" href="categorie.php">Ontbijt</a></li>
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="#">Lunch</a></li>
             <li><hr class="dropdown-divider"></li>
