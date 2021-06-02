1f761b2f05b68785c33d0c3096faf7771651479b
Philip <85866@roc-teraa.nl>
2021-06-02 14:22:24 +0200

    Update categorie.php

--- RPM-Project/categorie.php
+++ RPM-Project/categorie.php
@@ -6,17 +6,29 @@
 ?>
 
 <div class="main">
-    <h3 class="text-uppercase">Ontbijt</h3>
+    <!-- Shows which page you are on -->
+    <h1 class="text-uppercase">Ontbijt</h1>
 
+    <!-- First Recipe of the list -->
     <div class="custom-container">
-        <div class="custom-row">
-            
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-8 "><h3>Recipe</h3><h6>Descriptionof the Recipe</h6></div>
+          <div class="col-sm-4"><img src="img/Logo--RPM-ZW.png" alt=""></div>
+          </div>
+        <div class="row">
+          <div class="col-sm-4">Description</div>
+          <div class="col-sm-4">col-sm</div>
+          
         </div>
+      </div>
     </div>
+
+    <!-- Second Recipe of the list -->
     <div class="custom-container">
-        <div class="custom-row">
-            
-        </div>
+      <div class="custom-row">
+        
+      </div>
     </div>
 <?php
   // Get the HTML footer section
