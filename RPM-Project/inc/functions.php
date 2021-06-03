085f7fcf52d6fe5ec63a823a37a46d144b8a73a5
Ramon van Veghel <49039@roc-teraa.nl>
2021-06-03 09:03:16 +0200

    functions added GetLunch

--- RPM-Project/inc/functions.php
+++ RPM-Project/inc/functions.php
@@ -23,5 +23,58 @@ function DbConnect()
         return $conn;
     
 }
+// Get recipes from the database, from tables courses, courses_recipes and recipes
+function GetLunch()
+{
+    //connect to the database 
+    $conn = DbConnect();
+    //define empty array 
+    $lunch = [];
+    //define sql
 
+    $getLunchSQL= 
+    "
+    SELECT 
+    *
+    FROM 
+    courses_recipes, recipes
+    WHERE recipes.recipeId = courses_recipes.recipeId 
+    AND 
+    courses_recipes.courseId = 2
+    ";
+    //run / execute query
+    //this will result in a result set or
+    $result = $conn->query($getLunchSQL) or die($conn->error);
     //fetch the result to an associative array
+    $lunch = $result->fetch_all(MYSQLI_ASSOC);
+    //return the array
+    return $lunch;
+}
+// function that shows contents of an array
+function DisplayLunch()
+{   
+    $lunch = GetLunch();
+    foreach($lunch as $lunches)
+    {
+        ?>
+        <div class="recipe-container">
+          <?php
+               echo $lunches ["recipeName"] . $lunches["recipeDescription"] . $lunches["recipeTime"] . $lunches["recipeImage"];
+         
+           ?>
+             </br>
+                Een heerlijk ontbijtje
+            </div>
+          <?php
+    }
+}
+function MyDump($lunch, $die = false)
+{
+    echo "<pre>";
+    var_dump($lunch); 
+    echo"</pre>";
+    if($die == true)
+    {
+        die("DIED");
+    }
+a
